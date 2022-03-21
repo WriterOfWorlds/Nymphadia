12d6bc567b30f5018e9c566151019f4ef2354e3f
Ethan Alston <Kamalualston@gmail.com>
2022-03-20 19:13:18 -0700

    Added anti-cramming (for real)

---
+++ src/main/java/net/mcreator/nymphadia/procedures/AntiCrammingProcedure.java
@@ -0,0 +1,49 @@
+package net.mcreator.nymphadia.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
+
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Comparator;
+
+@Mod.EventBusSubscriber
+public class AntiCrammingProcedure {
+	@SubscribeEvent
+	public static void onEntityTick(LivingEvent.LivingUpdateEvent event) {
+		execute(event, event.getEntityLiving().level, event.getEntityLiving().getX(), event.getEntityLiving().getY(), event.getEntityLiving().getZ(),
+				event.getEntityLiving());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		double entitynum = 0;
+		entitynum = 0;
+		{
+			final Vec3 _center = new Vec3(x, y, z);
+			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(2 / 2d), e -> true).stream()
+					.sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).collect(Collectors.toList());
+			for (Entity entityiterator : _entfound) {
+				entitynum = entitynum + 1;
+			}
+		}
+		if (entitynum >= 5) {
+			entity.makeStuckInBlock(Blocks.AIR.defaultBlockState(), new Vec3(0.25, 0.05, 0.25));
+		}
+	}
+}
